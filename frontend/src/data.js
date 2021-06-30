7e84e417c1b3835aa2162cb04472febb59795569
JL <jennalbouche@gmail.com>
2021-06-30 10:08:54 -0700

    add react frontend

---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 120,
+            brand: 'Yeezy',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality shirt'
+        },
+        {
+            _id: '2',
+            name: 'Cool Shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 100,
+            brand: 'Shady',
+            rating: 4.2,
+            numReviews: 20,
+            description: 'High quality shirt'
+        },
+        {
+            _id: '3',
+            name: 'Goofy Shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 150,
+            brand: 'Fancy',
+            rating: 4.8,
+            numReviews: 37,
+            description: 'High quality shirt'
+        },
+        {
+            _id: '4',
+            name: 'Slim Pants',
+            category: 'Pants',
+            image: '/images/p4.jpg',
+            price: 135,
+            brand: 'Yeezy',
+            rating: 4.3,
+            numReviews: 15,
+            description: 'High quality pant'
+        },
+        {
+            _id: '5',
+            name: 'Cool Pants',
+            category: 'Pants',
+            image: '/images/p4.jpg',
+            price: 80,
+            brand: 'Shady',
+            rating: 4.5,
+            numReviews: 42,
+            description: 'High quality pant'
+        },
+        {
+            _id: '6',
+            name: 'Goofy Pants',
+            category: 'Pants',
+            image: '/images/p4.jpg',
+            price: 150,
+            brand: 'Goofy',
+            rating: 4.5,
+            numReviews: 13,
+            description: 'High quality pant'
+        }
+    ]
+}
+
+export default data;
